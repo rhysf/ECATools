e3501e9f809258e2b2daf3e55ed6851a4a687401
Rhys Farrer <3695082+rhysf@users.noreply.github.com>
2025-07-14 23:43:01 +0100

    avoid overwriting files/analysis already completed

--- util/VCF_to_variants_and_ref_sites.pl
+++ util/VCF_to_variants_and_ref_sites.pl
@@ -35,17 +35,21 @@ my $settings = "m-$opt_m-s-$opt_s-e-$opt_e-z-$opt_z";
 my $outfile1 = "$opt_o/$filename-$settings-reference-bases.tab";
 my $outfile2 = "$opt_o/$filename-$settings-variant-bases.tab";
 
-# Save sequences
-my $fasta = fastafile::fasta_to_struct($opt_f);
+if((-e $outfile1) || (-e $outfile2)) {
+        warn "Skipping as $outfile1 and/or $outfile2 already made\n";
+} else {
+        # Save sequences
+        my $fasta = fastafile::fasta_to_struct($opt_f);
 
-# Save genome to array
-my $genome_array = genomearray::make_genome_hash_array_from_seq_struct($fasta);
+        # Save genome to array
+        my $genome_array = genomearray::make_genome_hash_array_from_seq_struct($fasta);
 
-# Convert ref bases to 1 and variants to 2
-$genome_array = genomearray::fill_genome_hash_array_from_vcf($genome_array, $opt_v, $opt_s, $opt_e, $opt_z, $opt_m);
+        # Convert ref bases to 1 and variants to 2
+        $genome_array = genomearray::fill_genome_hash_array_from_vcf($genome_array, $opt_v, $opt_s, $opt_e, $opt_z, $opt_m);
 
-# Print tab files for locations of 1s (reference)
-genomearray::print_tab_file_from_regions_in_genome_hash($fasta, $genome_array, 1, $outfile1);
+        # Print tab files for locations of 1s (reference)
+        genomearray::print_tab_file_from_regions_in_genome_hash($fasta, $genome_array, 1, $outfile1);
 
-# Print tab files for locations of 1s (variant)
-genomearray::print_tab_file_from_regions_in_genome_hash($fasta, $genome_array, 2, $outfile2);
+        # Print tab files for locations of 1s (variant)
+        genomearray::print_tab_file_from_regions_in_genome_hash($fasta, $genome_array, 2, $outfile2);
+}
